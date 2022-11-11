550ac7f2988e09bab5df3d0008c34574ca7a5383
user <user@gmail.com>
2022-11-11 20:16:03 +0200

    Finish helmet.js tasks

--- myApp.js
+++ myApp.js
@@ -2,10 +2,39 @@ const express = require('express')
 const app = express()
 // My code start
 const helmet = require('helmet')
-app.use(helmet())
-app.use(helmet.hidePoweredBy());
-
-
+// app.use(helmet.hidePoweredBy())
+// app.use(helmet.frameguard({ action: 'deny' }))
+// app.use(helmet.xssFilter())
+// app.use(helmet.noSniff())
+// app.use(helmet.ieNoOpen())
+// let ninetyDaysInSeconds = 90 * 24 * 60 * 60
+// app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }))
+// app.use(helmet.dnsPrefetchControl())
+// app.use(helmet.noCache())
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: ["'self'", 'trusted-cdn.com'],
+//     },
+//   })
+// )
+app.use(
+  helmet({
+    frameguard: {
+      // configure
+      action: 'deny',
+    },
+    contentSecurityPolicy: {
+      // enable and configure
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ['style.com'],
+      },
+    },
+    dnsPrefetchControl: false, // disable
+  })
+)
 // My code end
 module.exports = app
 const api = require('./server.js')
