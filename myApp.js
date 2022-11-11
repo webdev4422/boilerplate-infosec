109ae276f876bff4bd2d36b4a323fc5806ee8fc1
webdev4422 <67459982+webdev4422@users.noreply.github.com>
2022-11-11 14:41:11 +0000

    Ahh helmet.js

--- myApp.js
+++ myApp.js
@@ -1,61 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+// My code start
+const helmet = require('helmet')
+app.use(helmet())
+app.use(helmet.hidePoweredBy());
 
 
+// My code end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
+module.exports = app
+const api = require('./server.js')
+app.use(express.static('public'))
+app.disable('strict-transport-security')
+app.use('/_api', api)
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/views/index.html')
+})
+let port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+  console.log(`Your app is listening on port ${port}`)
+})
